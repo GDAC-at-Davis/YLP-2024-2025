6b707e115b95220c3da4416b988a8b4fc3886a1f
Arthur G <arthurgan29@gmail.com>
2025-02-10 00:02:31 -0800

    Added punching bag and additional layers

--- YearLongProject/Assets/Scripts/GameEntities/EnvironmentEntity.cs
+++ YearLongProject/Assets/Scripts/GameEntities/EnvironmentEntity.cs
@@ -1,6 +1,12 @@
+using Hitbox.DataStructures;
+
 namespace GameEntities
 {
     public class EnvironmentEntity : Entity
     {
+        public override void OnHitByAttack(HitboxInstance hitboxInstance)
+        {
+            // Do nothing
+        }
     }
 }
