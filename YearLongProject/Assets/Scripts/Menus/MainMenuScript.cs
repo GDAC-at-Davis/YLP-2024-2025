699b83a33ac9581fbc2500cf70e5924b53c0b44a
Timothy Liu <timothys.new.email@gmail.com>
2025-02-27 13:48:41 -0800

    Implement Main Menu Barebones
    Added barebone functionality for main menu with scythe indicator. Placeholder functions live/laugh/love implemented in place for Live/Lore/Leave

---
+++ YearLongProject/Assets/Scripts/Menus/MainMenuScript.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // this script right now really should only control the scelect indication behavior and moving between scenes
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Image scelect; // scythe select object. I'll define all of scelects behavior when moving in here
+    public Animator animator;
+    private GameObject lastSelected;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        GameObject selected = EventSystem.current.currentSelectedGameObject;
+
+        // Check if the selection has changed
+        if (selected != lastSelected)
+        {
+            lastSelected = selected; // Update the last selected object
+            animator.SetTrigger("move");
+
+            // Only move if a valid object is selected
+            if (selected != null && selected.TryGetComponent<RectTransform>(out RectTransform buttonRect))
+            {
+                Vector3 buttonPosition = buttonRect.position;
+                scelect.transform.position = buttonPosition + new Vector3(-100, 0, 0);
+            }
+        }
+
+        // WOOOOOOOO selection checking
+        if (selected != null && Input.GetButtonDown("Submit"))
+        {
+            if (selected.TryGetComponent<Button>(out Button button))
+            {
+                animator.SetTrigger("select");
+            }
+        }
+    }
+
+    public void live(){
+        // transition to character select scene
+    }
+
+    public void lore(){
+        // transition to lore scene
+    }
+
+    public void leave(){
+        Application.Quit();
+    }
+
+    // scelect behavior functions
+    void scelect_move(){
+        
+    }
+}
