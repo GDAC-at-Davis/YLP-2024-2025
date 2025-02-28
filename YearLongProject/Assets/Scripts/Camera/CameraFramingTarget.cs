2b7a2f80198b778290f53523d4ef10337e1408a0
Arthur G <arthurgan29@gmail.com>
2025-02-27 17:48:28 -0800

    Removed camera framing on Punching Bags

--- YearLongProject/Assets/Scripts/Camera/CameraFramingTarget.cs
+++ YearLongProject/Assets/Scripts/Camera/CameraFramingTarget.cs
@@ -1,9 +1,11 @@
 using Base;
+using UnityEngine;
 
 namespace Camera
 {
     public class CameraFramingTarget : DescriptionMono
     {
+        [field: SerializeField]
         public bool IsTargeted { get; set; } = true;
     }
 }
