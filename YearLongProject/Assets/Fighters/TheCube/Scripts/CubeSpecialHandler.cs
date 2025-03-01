64d1908ddff1082aff7d14cebb3ca9b9cffa2f58
James Jasper Fadden O'Roarke <faddenjasper@gmail.com>
2025-02-28 17:53:21 -0800

    Created special move for cube

---
+++ YearLongProject/Assets/Fighters/TheCube/Scripts/CubeSpecialHandler.cs
@@ -0,0 +1,89 @@
+using Base;
+using Input_Scripts;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CharacterScripts
+{
+    /// <summary>
+    ///     Handles flipping the character based on input
+    /// </summary>
+    public class CubeSpecialHandler : DescriptionMono
+    {
+
+
+        public UnityEvent<bool> OnTrapSetChange;
+
+        public bool IsTrapSet = false;
+        public bool IsTrapActive = false;
+
+        private Vector3 startPos;
+        private Vector3 endPos;
+        private float trapDuration;
+        private float elapsedDuration;
+
+        Rigidbody2D playerRigidbody;
+
+
+        private void Start()
+        {
+
+            playerRigidbody = transform.parent.gameObject.GetComponentInChildren<Rigidbody2D>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            if (IsTrapActive)
+            {
+                elapsedDuration += Time.deltaTime;
+
+                float trapProgress = elapsedDuration / trapDuration;
+
+                endPos = playerRigidbody.transform.position + Vector3.up * 0.5f;
+
+                if (trapProgress < 1)
+                {
+                    transform.position = Vector3.Lerp(startPos, endPos, trapProgress);
+                }
+                
+            }
+        }
+
+
+
+        public void SetTrap()
+        {
+            gameObject.transform.position = playerRigidbody.transform.position + Vector3.up * 0.5f;
+
+            startPos = transform.position + Vector3.up * 0.5f;
+
+            
+            
+            IsTrapSet = true;
+        }
+
+        public void TriggerTrap(float duration)
+        {
+            IsTrapSet = false;
+            IsTrapActive = true;
+
+            elapsedDuration = 0;
+            trapDuration = duration;
+
+            endPos = playerRigidbody.transform.position + Vector3.up * 0.5f;
+
+
+
+        }
+        
+
+        public void EndTrap()
+        {
+            IsTrapActive = false;
+        }
+
+    }
+}
