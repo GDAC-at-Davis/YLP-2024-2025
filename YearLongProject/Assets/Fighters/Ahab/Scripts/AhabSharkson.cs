0c5398d2dd58c3642e782292a694b81d4b70a7e2
Arthur G <arthurgan29@gmail.com>
2025-05-21 18:46:38 -0700

    fixed shark facing

--- YearLongProject/Assets/Fighters/Ahab/Scripts/AhabSharkson.cs
+++ YearLongProject/Assets/Fighters/Ahab/Scripts/AhabSharkson.cs
@@ -224,6 +224,11 @@ namespace Fighters.Ahab.Scripts
 
         private void AlignSharkToVel()
         {
+            if (rb.linearVelocity.x == 0)
+            {
+                return;
+            }
+
             bool flipX = rb.linearVelocity.x < 0;
             sprite.transform.localScale = new Vector3(flipX ? -1 : 1, 1, 1);
         }
