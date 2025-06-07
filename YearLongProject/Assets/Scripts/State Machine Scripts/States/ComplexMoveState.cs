2156ffa1453fb25d8dd7ef2a7e7495a34883dcf0
endawghae <94265058+endawghae@users.noreply.github.com>
2025-06-07 00:56:25 -0700

    wizard wip

---
+++ YearLongProject/Assets/Scripts/State Machine Scripts/States/ComplexMoveState.cs
@@ -0,0 +1,143 @@
+using EditorUtils.BoldHeader;
+using Movement;
+using NaughtyAttributes;
+using Timeline;
+using UnityEngine;
+
+namespace State_Machine_Scripts.States
+{
+    public class ComplexMoveState : CharacterState
+    {
+        private enum MoveSubStates
+        {
+            Idle,
+            EnterMoving,
+            Moving,
+            Airborne
+        }
+
+        [BoldHeader("Basic Movement State")]
+        [InfoBox("Basic movement state for running, jumping, and air.")]
+        [Header("MoveState Config")]
+
+        [SerializeField]
+        private SimpleMovementController movementController;
+
+        [SerializeField]
+        private CharacterRigidbody2D characterRigidbody;
+
+        [SerializeField]
+        private ManualTimelinePlayer enterMovingPlayableAsset;
+
+        [SerializeField]
+        private ManualTimelinePlayer movingPlayableAsset;
+
+        [SerializeField]
+        private ManualTimelinePlayer idlePlayableAsset;
+
+        [SerializeField]
+        private ManualTimelinePlayer airPlayableAsset;
+
+        [SerializeField]
+        private CharacterState jumpState;
+
+        private MoveSubStates currentSubState;
+        private ManualTimelinePlayer currentPlayableAsset;
+
+        private void Update()
+        {
+            Vector2 moveInput = ActionManager.CharacterActionInput.MoveInput;
+            movementController.SetHorizontalInput(moveInput.x);
+            SelectMoveState(moveInput);
+            if (jumpState)
+            {
+                ActionManager.SetActionTypeAllowed(jumpState.StateName, movementController.GetIsGrounded());
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (currentPlayableAsset != null)
+            {
+                currentPlayableAsset.Evaluate(ActionManager.FixedDeltaTime);
+            }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            currentPlayableAsset = null;
+            currentSubState = (MoveSubStates)(-1);
+            SelectMoveState(Vector2.zero);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            currentPlayableAsset?.Stop();
+            currentPlayableAsset = null;
+        }
+
+        private void SelectMoveState(Vector2 moveInput)
+        {
+            if (movementController.GetIsGrounded())
+            {
+                if (Mathf.Abs(characterRigidbody.LinearVelocity.x) < 0.5f && moveInput.x == 0)
+                {
+                    SetSubState(MoveSubStates.Idle);
+                }
+                else if (currentSubState != MoveSubStates.Moving)
+                {
+                    SetSubState(MoveSubStates.EnterMoving);
+                }
+                else
+                {
+                    SetSubState(MoveSubStates.Moving);
+                }
+            }
+            else
+            {
+                SetSubState(MoveSubStates.Airborne);
+            }
+        }
+        public void OnEnterMoveEnd()
+        {
+            SetSubState(MoveSubStates.Moving);
+        }
+
+        private void SetSubState(MoveSubStates subState)
+        {
+            if (currentSubState == subState)
+            {
+                return;
+            }
+
+            currentSubState = subState;
+
+            currentPlayableAsset?.Stop();
+
+            switch (subState)
+            {
+                case MoveSubStates.Idle:
+                    currentPlayableAsset = idlePlayableAsset;
+                    break;
+                case MoveSubStates.EnterMoving:
+                    currentPlayableAsset = enterMovingPlayableAsset;
+                    break;
+                case MoveSubStates.Moving:
+                    currentPlayableAsset = movingPlayableAsset;
+                    break;
+                case MoveSubStates.Airborne:
+                    currentPlayableAsset = airPlayableAsset;
+                    break;
+                default:
+                    Debug.LogError("Invalid MoveSubState");
+                    break;
+            }
+
+            currentPlayableAsset.Play();
+        }
+    }
+}
